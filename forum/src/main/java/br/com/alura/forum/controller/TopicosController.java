39438109606786049fce7020710c2771ebf169f8
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-11 11:18:34 -0300

    create topicorepository + get topico

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +14,17 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @GetMapping("/topicos")
-    public List<TopicoDto> lista() {
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+        if(nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
 }
---
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCurso_Nome(String nomeCurso);
+
+    // Segunda forma sem utilizar diretamente o spring data
+   /* @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso ")
+    List<Topico> carregarPorNomedoCurso(@Param("nomeCurso") String nomeCurso); */
+
+}
