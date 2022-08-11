02578efb827a831bdd248f1d7ee188f6e4300cd1
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-11 08:31:33 -0300

    first commit

---
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @GetMapping("/topicos")
+    public List<Topico> lista() {
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+        return Arrays.asList(topico, topico, topico);
+    }
+}
