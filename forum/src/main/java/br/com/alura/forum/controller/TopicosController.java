3f71956e880e1f4872fcb1f13f946a5b7353193d
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-11 11:42:28 -0300

    create cursoRepository + post topico

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,27 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
 
-    @GetMapping("/topicos")
+   @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
         if(nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
@@ -26,5 +30,10 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
             return TopicoDto.converter(topicos);
         }
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form) {
+        Topico topico = form.converter(cursoRepository);
     }
 }
