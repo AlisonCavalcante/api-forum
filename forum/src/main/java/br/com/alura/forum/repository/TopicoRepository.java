56ce0a004f44484be12413414e28f79df2506c0c
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-12 21:53:02 -0300

    add paginacao simples

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -8,7 +10,7 @@ import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCurso_Nome(String nomeCurso);
+    Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 
     // Segunda forma sem utilizar diretamente o spring data
    /* @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso ")
